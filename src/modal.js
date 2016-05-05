5bafd62d875b1a884168a5ad2009c167db3b6b27
Eugene Trounev <eugene.trounev@gmail.com>
2016-05-05 00:11:25 -0400

    Update the alert template a bit. Allow closing by ESC

--- src/modal.js
+++ src/modal.js
@@ -10,14 +10,13 @@ angular.module('com.likalo.ui')
             plain: true,
             closeByNavigation: true,
             className: 'UI-modal UI-modal--alert',
-            template: '<aside class="UI-info-block--alert"><svg class="UI-info-block--decorator"><rect class="UI-decorator--base" height="100%"></rect><use xmlns:xlink="http://www.w3.org/1999/xlink" class="UI-decorator--icon" ui-icon-xlink-href="alert" xlink:href="#icon--alert"></use></svg><p class="UI-info-block--content"><strong>' + title + '</strong><br/>' + message + '</p></aside>'
+            template: '<aside class="UI-info-block--alert"><svg class="UI-info-block--decorator"><rect class="UI-decorator--base" height="100%"></rect><use xmlns:xlink="http://www.w3.org/1999/xlink" class="UI-decorator--icon" xlink:href="#icon--block-alert"></use></svg><p class="UI-info-block--content"><strong>' + title + '</strong><br/>' + message + '</p></aside>'
           });
         },
         modal: function (template, controller, more) {
           return ngDialog.openConfirm(angular.extend({
-            showClose: false,
-            closeByEscape: false,
             closeByDocument: false,
+            closeByNavigation: true,
             className: 'UI-modal UI-modal--dialog',
             template: template,
             controller: controller
@@ -26,8 +25,8 @@ angular.module('com.likalo.ui')
         view: function (template, controller, more) {
           return ngDialog.open(angular.extend({
             showClose: false,
-            closeByEscape: true,
-            closeByDocument: true,
+            closeByEscape: false,
+            closeByDocument: false,
             closeByNavigation: true,
             className: 'UI-modal UI-modal--view',
             template: template,
