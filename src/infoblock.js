d942109ef4f1a1072c65f6db54f944ff050e0ec1
Eugene Trounev <eugene.trounev@gmail.com>
2016-05-05 00:10:57 -0400

    Do not bolck the scope

--- src/infoblock.js
+++ src/infoblock.js
@@ -4,12 +4,9 @@ angular.module('com.likalo.ui')
    replace: true,
    restrict: 'E',
    transclude: true,
-   scope: {
-    type: "@"
+   link: function($scope, $el, $attrs) {
+     $scope.type = $attrs['type'] || 'info';
    },
-   controller: function($scope) {
-     $scope.type = $scope['type'] || 'info';
-   },
-   template: '<aside class="UI-info-block UI-info-block--{{type}}"><svg class="UI-info-block--decorator"><rect class="UI-decorator--base" height="100%"/><use xmlns:xlink="http://www.w3.org/1999/xlink" class="UI-decorator--icon" ui-icon-xlink-href="{{type}}" xlink:href=""></use></svg><p class="UI-info-block--content" ng-transclude></p></aside>'
+   template: '<aside class="UI-info-block UI-info-block--{{type}}"><svg class="UI-info-block--decorator"><rect class="UI-decorator--base" height="100%"/><use xmlns:xlink="http://www.w3.org/1999/xlink" class="UI-decorator--icon" ui-icon-xlink-href="block-{{type}}" xlink:href=""></use></svg><p class="UI-info-block--content" ng-transclude></p></aside>'
   };
  }]);
